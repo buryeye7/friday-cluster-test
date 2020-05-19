3b796ff58a2e977d6834e48631143f873bfac837
Jaehyun Lee <buryeye7@naver.com>
2020-05-19 01:07:57 +0000

    a

--- dockerfiles/build-and-push.sh
+++ dockerfiles/build-and-push.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cd hdac-seed
+pwd
 docker build --no-cache --tag buryeye7/hdac-seed:latest .
 docker push buryeye7/hdac-seed:latest
 
 cd ..
 cd hdac-node
+pwd
 docker build --no-cache --tag buryeye7/hdac-node:latest .
 docker push buryeye7/hdac-node:latest
 
