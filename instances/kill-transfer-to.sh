a0fdd09b32db2a40924630ab759ea0cedb893b09
Jaehyun Lee <buryeye7@naver.com>
2020-08-12 05:06:49 +0000

    add

--- instances/kill-transfer-to.sh
+++ instances/kill-transfer-to.sh
@@ -4,6 +4,9 @@ ps -ef | grep transfer > /tmp/dummy.txt
 while read line
 do
     echo $line
+    if [[ $line == *"auto"* ]];then
+        continue
+    fi
     if [[ $line == *"transfer"* ]];then
         process=$(echo $line | awk -F' ' '{print $2}')
         kill -9 $process
